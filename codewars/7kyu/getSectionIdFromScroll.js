3326643deb9cf000808772b67e9a12e5109b817a
Mustafa Ozturk <mozturkpro@gmail.com>
2021-09-19 20:07:44 -0400

    fix

--- codewars/7kyu/getSectionIdFromScroll.js
+++ codewars/7kyu/getSectionIdFromScroll.js
@@ -1,13 +1,13 @@
 // https://www.codewars.com/kata/5cb05eee03c3ff002153d4ef
 function getSectionIdFromScroll(scrollY,sizes){
-    let count = 0;
+    let index = 0;
      for (let i = 0; i < sizes.length; i++) {
        if(scrollY >= sizes[i]) {
-         count += 1;
+         index += 1;
          scrollY -= sizes[i]
        } else {
          break;
        }
      }
-    return count >= sizes.length ? -1 : count;
+    return index >= sizes.length ? -1 : index;
 }
