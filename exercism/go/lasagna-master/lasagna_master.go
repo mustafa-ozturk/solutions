114ccc6af50727128f3b4bfbc4f256f5faabb1af
Mustafa Ozturk <mozturkpro@gmail.com>
2022-02-15 10:01:57 -0500

    go exercism

---
+++ exercism/go/lasagna-master/lasagna_master.go
@@ -0,0 +1,37 @@
+package lasagna
+
+// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, prepTime int) int {
+	if prepTime == 0 {
+		prepTime = 2
+	}
+	return len(layers) * prepTime
+}
+
+// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	noodles := 0
+	sauce := 0.0
+	for _, layer := range layers {
+		if layer == "noodles" {
+			noodles += 50
+		} else if layer == "sauce" {
+			sauce += 0.2
+		}
+	}
+	return noodles, sauce
+}
+
+// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendList []string, myList []string) {
+	myList[len(myList)-1] = friendList[len(friendList)-1]
+}
+
+// TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(amounts []float64, portion int) []float64 {
+	scaledQuantities := make([]float64, len(amounts))
+	for i := 0; i < len(amounts); i++ {
+		scaledQuantities[i] = amounts[i] * (float64(portion) / 2)
+	}
+	return scaledQuantities
+}
