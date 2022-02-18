645dea1ebee8eaadae4bfb1db6701317d296a1ac
Mustafa Ozturk <mozturkpro@gmail.com>
2022-02-18 09:01:28 -0500

    exercism go

---
+++ exercism/go/elons-toys/elons_toys.go
@@ -0,0 +1,44 @@
+package elon
+
+import "fmt"
+
+// Car implements a remote controlled car.
+type Car struct {
+	speed        int
+	batteryDrain int
+
+	battery  int
+	distance int
+}
+
+// NewCar creates a new car with given specifications.
+func NewCar(speed, batteryDrain int) *Car {
+	return &Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+	}
+}
+
+// TODO: define the 'Drive()' method
+func (car *Car) Drive() {
+	if car.battery >= car.batteryDrain {
+		car.distance = car.speed
+		car.battery -= car.batteryDrain
+	}
+}
+
+// TODO: define the 'DisplayDistance() string' method
+func (car Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
+
+// TODO: define the 'DisplayBattery() string' method
+func (car Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", car.battery)
+}
+
+// TODO: define the 'CanFinish(trackDistance int) bool' method
+func (car Car) CanFinish(trackDistance int) bool {
+	return (trackDistance/car.speed)*car.batteryDrain <= car.battery
+}
