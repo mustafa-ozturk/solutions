b539c273db3ef24c339334a743f99cbb94dd932b
Mustafa Ozturk <mozturkpro@gmail.com>
2022-02-19 19:37:19 -0500

    improvement go

--- exercism/go/the-farm/the_farm.go
+++ exercism/go/the-farm/the_farm.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 )
 
-// See types.go for the types defined for this exercise.
+type WeightFodder interface {
+	FodderAmount() (float64, error)
+}
+
+var ErrScaleMalfunction = errors.New("sensor error")
 
 // TODO: Define the SillyNephewError type here.
 type SillyNephewError struct {
@@ -19,23 +23,25 @@ func (e *SillyNephewError) Error() string {
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
 	fodder, err := weightFodder.FodderAmount()
-	if err == ErrScaleMalfunction && fodder > 0 {
-		return (fodder * 2.0) / float64(cows), nil
+
+	if err == ErrScaleMalfunction {
+		fodder *= 2
+	} else if err != nil {
+		return 0.0, err
 	}
-	if err == ErrScaleMalfunction && fodder < 0 {
-		return 0, errors.New("negative fodder")
-	}
-	if err != nil {
-		return 0, err
-	}
-	if cows < 0 {
-		return 0, &SillyNephewError{cows}
-	}
-	if cows == 0 {
-		return 0, errors.New("division by zero")
-	}
+
 	if fodder < 0 {
-		return 0, errors.New("negative fodder")
+		return 0.0, errors.New("negative fodder")
+	}
+
+	if cows < 0 {
+		return 0.0, &SillyNephewError{cows}
+	} else if cows == 0 {
+		return 0.0, errors.New("division by zero")
+	}
+
+	if err == ErrScaleMalfunction && fodder < 0 {
+		return 0.0, errors.New("negative fodder")
 	}
 
 	return fodder / float64(cows), nil
